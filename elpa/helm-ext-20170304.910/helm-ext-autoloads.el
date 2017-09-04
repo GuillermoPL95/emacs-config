68ed1a8e6ba8c6d1cffb7d895230da4dda1d507c
Guillermo Paredes Lopez <guillermo.paredes.lopez@alumnos.upm.es>
2017-09-04 10:52:17 +0200

    first config

---
+++ elpa/helm-ext-20170304.910/helm-ext-autoloads.el
@@ -0,0 +1,42 @@
+;;; helm-ext-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil "helm-ext" "helm-ext.el" (22724 10586 952237
+;;;;;;  756000))
+;;; Generated autoloads from helm-ext.el
+
+(autoload 'helm-ext-ff-enable-zsh-path-expansion "helm-ext" "\
+
+
+\(fn ENABLE)" t nil)
+
+(autoload 'helm-ext-ff-enable-auto-path-expansion "helm-ext" "\
+
+
+\(fn ENABLE)" t nil)
+
+(autoload 'helm-ext-ff-enable-skipping-dots "helm-ext" "\
+
+
+\(fn ENABLE)" t nil)
+
+(autoload 'helm-ext-minibuffer-enable-header-line-maybe "helm-ext" "\
+
+
+\(fn ENABLE)" nil nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("helm-ext-ff.el" "helm-ext-minibuffer.el"
+;;;;;;  "helm-ext-pkg.el") (22724 10587 64964 517000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; helm-ext-autoloads.el ends here
---
+++ elpa/helm-spotify-plus-20170311.221/helm-spotify-plus-pkg.el
@@ -0,0 +1,1 @@
+(define-package "helm-spotify-plus" "20170311.221" "Control Spotify, search and select music with Helm." '((emacs "24.4") (helm "2.0.0") (multi "2.0.1")))
